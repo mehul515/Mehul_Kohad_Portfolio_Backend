4eb0c8af5f81bdf36dc16675aea96d4234ae33f1
mehul515 <147683942+mehul515@users.noreply.github.com>
2024-06-26 14:12:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: "*",
+    origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
